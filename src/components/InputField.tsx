cde9b412ae048ee5072a3b294599fbc49ba983f3
Paul <paulbmorrell@gmail.com>
2024-12-31 16:28:05 -0500

    added useState hook

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
   return (
     <form className="input">
-        <input type="input" className="input_field" placeholder="Start typing..." />
+        <input type="input" 
+            className="input_field" 
+            placeholder="Start typing..." 
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+        />
         <button className="input__submit" type="submit">Add</button>
     </form>
   );
-}
+};
 
 export default InputField;
