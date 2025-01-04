e446b2fcbb0a7252b0b834fa2b05bb57efd136c7
Paul <paulbmorrell@gmail.com>
2025-01-03 21:44:57 -0500

    added function to handle completing an note item

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
 import './styles.css';
+import React, { useRef } from 'react';
 
 interface Props {
     todo: string,
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -13,11 +13,22 @@ type Props = {
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
 
+    const handleDone = (id: number) => {
+        setTodos(
+            todos.map((todo)=>
+                todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+        )) 
+    };
+
     return (
         <form className="todos__single">
-            <span className="todos__single--text">
-                {todo.todo}            
-            </span>
+            {
+                todo.isDone? (
+                    <s className="todos__single--text">{todo.todo}</s>
+                ):(
+                    <span className="todos__single--text">{todo.todo}</span>
+                )
+            }
             <div>
                 <span className="icon">
                     <AiFillEdit />
@@ -25,7 +36,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
                 <span className="icon">
                     <AiFillDelete />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDone(todo.id)}>
                     <MdDone />
                 </span>
             </div>
