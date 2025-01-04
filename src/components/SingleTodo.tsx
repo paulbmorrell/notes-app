d86080488a47de9fafde741617e3bc4f00eb835f
Paul <paulbmorrell@gmail.com>
2025-01-03 21:56:32 -0500

    implement function for delete using filter

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -13,6 +13,10 @@ type Props = {
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
 
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
     const handleDone = (id: number) => {
         setTodos(
             todos.map((todo)=>
@@ -33,7 +37,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
                 <span className="icon">
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDelete(todo.id)}>
                     <AiFillDelete />
                 </span>
                 <span className="icon" onClick={() => handleDone(todo.id)}>
